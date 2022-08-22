e0aa38c4d1a99eb2b6dfc7415fda897cd8420c0c
Sigve Nordgaard <sigve.nordgaard@nsd.no>
2022-08-22 13:29:21 +0200

    Add example diffs

--- src/main.clj
+++ src/main.clj
@@ -1,7 +1,12 @@
 (ns main
   (:require [clojure.edn :as edn]
+            [clojure.data.json :as json]
             [clojure.java.io :as io]
             [editscript.core :as editscript]))
+
+(defn write-diff [diff path]
+  (with-open [w (io/writer path)]
+    (.write w (json/write-str (editscript/get-edits diff)))))
 
 (defn read-edn [path]
   (with-open [r (io/reader path)]
@@ -11,19 +16,22 @@
 (def new (read-edn "new.edn"))
 
 (println "Running the vanilla diff algorithm")
-(time (some? (editscript/diff old
-                              new
-                              {;:algo :quick
-                               :str-diff? true})))
+(let [diff (time (editscript/diff old
+                                  new
+                                  {;:algo :quick
+                                   :str-diff? true}))]
+  (write-diff diff "out/diff.json"))
 
 (println "Running the :quick diff algorithm, but without the :active field in 'old'")
-(time (some? (editscript/diff (map #(dissoc % :active) old)
-                              new
-                              {:algo :quick
-                               :str-diff? true})))
+(let [diff (time (editscript/diff (map #(dissoc % :active) old)
+                                  new
+                                  {:algo :quick
+                                   :str-diff? true}))]
+  (write-diff diff "out/diff_fields.json"))
 
 (println "Running the :quick diff algorithm")
-(time (some? (editscript/diff old
-                              new
-                              {:algo :quick
-                               :str-diff? true})))
+(let [diff (time (editscript/diff old
+                                  new
+                                  {:algo :quick
+                                   :str-diff? true}))]
+  (write-diff diff "out/diff_quick.json"))
